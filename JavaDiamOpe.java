4d1593a9d538f3c9c0ca6d0516deba7cff30e849
SwathiPriya2000 <63891711+SwathiPriya2000@users.noreply.github.com>
2020-10-02 21:08:21 +0530

    Add files via upload

---
+++ JavaDiamOpe.java
@@ -0,0 +1,16 @@
+abstract class MyClass<T>{  
+    abstract T add(T num, T num2);  
+}  
+public class JavaDiamOpe {  
+    public static void main(String[] args) {  
+        MyClass<Integer> obj = new MyClass<Integer>() {  
+            Integer add(Integer x, Integer y) {  
+                return x+y;   
+            }  
+        };    
+        Integer sum = obj.add(100,101);  
+        System.out.println(sum);  
+    }  
+}
+
+
